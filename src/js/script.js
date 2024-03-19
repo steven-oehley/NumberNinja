2765d06f2729e7326fabaa8fd232e13a3db179c6
Steve <steve.gitprofile@outlook.com>
2024-03-19 13:34:22 +0200

    refactored setting content and applying styles into functions - DRY principle

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,12 @@
 'use strict';
 
+// generate random num function
+function getRandomNum() {
+  return Math.floor(Math.random() * 20) + 1;
+}
+
 // create and set random ninja's number on page
-let ninjaNumber = Math.floor(Math.random() * 20) + 1;
+let ninjaNumber = getRandomNum();
 
 // Get important DOM elements
 const messageEl = document.querySelector('.message');
@@ -13,53 +18,72 @@ const highScoreEl = document.querySelector('.highscore');
 const userGuessEl = document.querySelector('.guess');
 let gameScore = parseInt(gameScoreEl.textContent);
 
+function setContent(element, content) {
+  element.textContent = content;
+}
+
+function applyStyle(element, styleType, styleProperty) {
+  if (element === document.body) {
+    document.body.style[styleType] = styleProperty;
+  } else {
+    element.style[styleType] = styleProperty;
+  }
+}
+
 checkBtn.addEventListener('click', () => {
   const userGuess = parseInt(userGuessEl.value);
   console.log(userGuess);
 
   if (!userGuess) {
-    messageEl.textContent = '⛔ No number selected!';
+    setContent(messageEl, '⛔ No number selected!');
   } else if (userGuess === ninjaNumber) {
     parseInt(highScoreEl.textContent) > gameScore
       ? (highScoreEl.textContent = highScoreEl.textContent)
       : (highScoreEl.textContent = gameScore);
-    ninjaNumberEl.textContent = ninjaNumber;
-    messageEl.textContent = "🥳 You guessed the Ninjas's number! 🥷";
-    ninjaNumberEl.style.width = '30rem';
-    ninjaNumberEl.style.color = '#de0500';
-    againBtn.style.backgroundColor = '#de0500';
-    againBtn.style.color = '#fff';
-    document.body.style.background = `    
-    linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)
-`;
 
-    document.body.style.backgroundSize = 'cover';
+    setContent(ninjaNumberEl, ninjaNumber);
+    setContent(messageEl, "🥳 You guessed the Ninjas's number! 🥷");
+
+    applyStyle(ninjaNumberEl, 'width', '30rem');
+    applyStyle(ninjaNumberEl, 'color', '#de0500');
+    applyStyle(againBtn, 'backgroundColor', '#de0500');
+    applyStyle(againBtn, 'color', '#fff');
+    applyStyle(
+      document.body,
+      'background',
+      `
+    linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)
+`
+    );
+    applyStyle(document.body, 'backgroundSize', 'cover');
   } else if (userGuess !== ninjaNumber) {
     if (gameScore > 1) {
-      messageEl.textContent =
+      let message =
         userGuess > ninjaNumber
           ? '☝️ Guess was too high!'
           : '👇 Guess was too low!';
+      setContent(messageEl, message);
       gameScore--;
-      gameScoreEl.textContent = gameScore;
+      setContent(gameScoreEl, gameScore);
     } else {
-      messageEl.textContent = "You lost the Ninja's Game 💀!";
+      setContent(messageEl, "You lost the Ninja's Game 💀!");
       gameScore--;
-      gameScoreEl.textContent = gameScore;
+      setContent(gameScoreEl, gameScore);
     }
   }
 });
 
 againBtn.addEventListener('click', () => {
-  ninjaNumber = Math.floor(Math.random() * 20) + 1;
+  ninjaNumber = getRandomNum();
   gameScore = 20;
   gameScoreEl.textContent = gameScore;
-  messageEl.textContent = 'Start guessing...';
-  ninjaNumberEl.textContent = '?';
-  ninjaNumberEl.style.color = '#222';
-  ninjaNumberEl.style.width = '15rem';
+  setContent(gameScoreEl, gameScore);
+  setContent(messageEl, 'Start guessing...');
+  setContent(ninjaNumberEl, '?');
+  applyStyle(ninjaNumberEl, 'color', '#222');
+  applyStyle(ninjaNumberEl, 'width', '15rem');
   userGuessEl.value = '';
-  document.body.style.background = '#222';
-  againBtn.style.backgroundColor = '#fff';
-  againBtn.style.color = '#222';
+  applyStyle(document.body, 'background', '#222');
+  applyStyle(againBtn, 'backgroundColor', '#fff');
+  applyStyle(againBtn, 'color', '#222');
 });
